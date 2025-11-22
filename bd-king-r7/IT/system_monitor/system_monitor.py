4c6ca7fb17d5332dfbc30d4600f2a7bf36ddc802
tamanna456760-it <242804952+tamanna456760-it@users.noreply.github.com>
2025-11-22 22:28:24 +0400

    Create system_monitor/system_monitor.py

---
+++ bd-king-r7/IT/system_monitor/system_monitor.py
@@ -0,0 +1,22 @@
+# System monitoring for security
+import psutil
+import logging
+
+class SystemMonitor:
+    def __init__(self):
+        self.logger = logging.getLogger('SystemMonitor')
+    
+    def monitor_services(self):
+        """Monitor running services"""
+        services = []
+        for proc in psutil.process_iter(['pid', 'name', 'status']):
+            services.append(proc.info)
+        return services
+    
+    def check_system_health(self):
+        """Check system resource usage"""
+        return {
+            'cpu_percent': psutil.cpu_percent(),
+            'memory_usage': psutil.virtual_memory().percent,
+            'disk_usage': psutil.disk_usage('/').percent
+        }
