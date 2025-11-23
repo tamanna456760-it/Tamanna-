0772c79a6edcbf871235ec890aa9d5d7f01af044
tamanna456760-it <242804952+tamanna456760-it@users.noreply.github.com>
2025-11-23 22:14:10 +0400

    Create apps.jsx

---
+++ bd-king-r7/website/frontend/src/apps.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Chat from './pages/Chat'
+import Features from './pages/Features'
+import About from './pages/About'
+import APIDocs from './pages/APIDocs'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/api-docs" element={<APIDocs />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default App
