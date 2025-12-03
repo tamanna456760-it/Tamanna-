0e8cbe106c9d853f7be1a89d51a631de977f537b
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2025-12-03 23:12:44 +0600

    Create python.py

---
+++ bd-king-r7/IT/bd-king-r7/powerhub/defanging/python.py
@@ -0,0 +1,9 @@
+import logging
+logger = logging.getLogger('powerhub')
+handler = logging.FileHandler('powerhub.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+logger.info("User login attempt", extra={"ip": request.remote_addr})
