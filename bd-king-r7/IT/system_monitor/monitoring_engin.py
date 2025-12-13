be73a882357a661d5f4abe29f45c7a8eda471215
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2025-12-13 20:07:02 +0400

    Create monitoring_engin.py

---
+++ bd-king-r7/IT/system_monitor/monitoring_engin.py
@@ -0,0 +1,20 @@
+# Tamanna Monitoring Engine
+import time
+
+class MonitoringEngine:
+    def __init__(self):
+        self.status = {}
+
+    def log(self, module, state: bool):
+        self.status[module] = state
+        print(f"[MONITOR] {module} => {'TRUE' if state else 'FALSE'}")
+
+    def heartbeat(self):
+        while True:
+            print("[HEARTBEAT] System alive...")
+            time.sleep(5)
+
+if __name__ == "__main__":
+    monitor = MonitoringEngine()
+    monitor.log("TamannaCore", True)
+    monitor.heartbeat()
