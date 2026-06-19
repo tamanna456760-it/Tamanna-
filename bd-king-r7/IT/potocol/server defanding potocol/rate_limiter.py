b0101d9552872bc837096d93e70bd0edb53e1d5e
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2026-06-19 21:57:47 +0400

    Create rate_limiter.py

---
+++ bd-king-r7/IT/potocol/server defanding potocol/rate_limiter.py
@@ -0,0 +1,21 @@
+import time
+
+class RateLimiter:
+    def __init__(self, limit=30):
+        self.limit = limit
+        self.requests = {}
+
+    def allow(self, ip):
+        now = time.time()
+
+        self.requests.setdefault(ip, [])
+        self.requests[ip] = [
+            t for t in self.requests[ip]
+            if now - t < 60
+        ]
+
+        if len(self.requests[ip]) >= self.limit:
+            return False
+
+        self.requests[ip].append(now)
+        return True
