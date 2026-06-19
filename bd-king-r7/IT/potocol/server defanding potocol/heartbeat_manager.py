85433c19b1398c56d7fa6fae95ba391835691e80
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2026-06-19 22:00:19 +0400

    Create heartbeat_manager.py

---
+++ bd-king-r7/IT/potocol/server defanding potocol/heartbeat_manager.py
@@ -0,0 +1,15 @@
+import time
+
+class HeartbeatManager:
+    def __init__(self):
+        self.nodes = {}
+
+    def update(self, node):
+        self.nodes[node] = time.time()
+
+    def online_nodes(self, timeout=60):
+        now = time.time()
+        return [
+            n for n, t in self.nodes.items()
+            if now - t < timeout
+        ]
