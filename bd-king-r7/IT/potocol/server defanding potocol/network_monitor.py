ea5ced80ede64362fa88fce930527a88cc0e034a
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2026-06-19 21:59:04 +0400

    Create network_monitor.py

---
+++ bd-king-r7/IT/potocol/server defanding potocol/network_monitor.py
@@ -0,0 +1,12 @@
+import socket
+
+def check_port(host, port):
+    try:
+        s = socket.create_connection(
+            (host, port),
+            timeout=3
+        )
+        s.close()
+        return True
+    except:
+        return False
