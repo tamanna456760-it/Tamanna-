1b3537489e1a180127767b1457efdafbaa42c998
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2026-06-19 21:58:18 +0400

    Create event_logger.py

---
+++ bd-king-r7/IT/potocol/server defanding potocol/event_logger.py
@@ -0,0 +1,13 @@
+import logging
+from pathlib import Path
+
+Path("logs").mkdir(exist_ok=True)
+
+logging.basicConfig(
+    filename="logs/server.log",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
+
+def log_event(msg):
+    logging.info(msg)
