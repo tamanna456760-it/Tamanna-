5d7bdaa073f7d30fdf1ae3b526fed51dd9a557a2
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2026-06-21 01:00:43 +0400

    Create tamanna.js

---
+++ Tamanna-AI-System/tamanna.js
@@ -0,0 +1,20 @@
+import puppeteer from "@cloudflare/puppeteer";
+export default {
+  async fetch(request, env) {
+    const { searchParams } = new URL(request.url);
+    const url = searchParams.get("url");
+    if (url) {
+      const browser = await puppeteer.launch(env.MYBROWSER);
+      const page = await browser.newPage();
+      await page.goto(url);
+      const img = await page.screenshot();
+      await browser.close();
+      return new Response(img, {
+        headers: {
+          "content-type": "image/jpeg",
+        },
+      });
+    } else {
+      return new Response("Please add an ?url=https://wwwtamanna.com/ parameter");
+    }
+  },
