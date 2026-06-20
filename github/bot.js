b08c4eb236a5152a16b59339e93a447153ada4cb
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2026-06-21 01:54:38 +0400

    Update bot.js

--- github/bot.js
+++ github/bot.js
@@ -1,7 +1,7 @@
 import { Octokit } from "@octokit/rest";
 
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN
+  auth: process.env.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
 });
 
 async function run() {
