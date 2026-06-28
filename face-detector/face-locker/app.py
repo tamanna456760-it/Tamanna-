d516f3f48d8fc8e1b92ded8de48169d436951319
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2026-06-28 21:17:40 +0400

    Create app.py

---
+++ face-detector/face-locker/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+from face_auth import verify_face
+from encryptor import decrypt_file, encrypt_file
+
+app = Flask(__name__)
+
+KNOWN_FACE = "database/my_face.jpg"
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+@app.route("/unlock", methods=["POST"])
+def unlock():
+
+    file = request.files["image"]
+    file.save("live.jpg")
+
+    if verify_face(KNOWN_FACE, "live.jpg"):
+        return {"status": "access granted"}
+    else:
+        return {"status": "denied"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
