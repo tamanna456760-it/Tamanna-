37df792e8195ba928766aeedcf933be9d62f3b56
HM INSAN ALI <242804952+tamanna456760-it@users.noreply.github.com>
2026-06-28 21:16:17 +0400

    Create face-locker/face_auth.py

---
+++ face-detector/face-locker/face_auth.py
@@ -0,0 +1,16 @@
+import face_recognition
+import cv2
+
+def verify_face(known_image, live_image):
+    known = face_recognition.load_image_file(known_image)
+    live = face_recognition.load_image_file(live_image)
+
+    known_enc = face_recognition.face_encodings(known)
+    live_enc = face_recognition.face_encodings(live)
+
+    if len(known_enc) == 0 or len(live_enc) == 0:
+        return False
+
+    result = face_recognition.compare_faces([known_enc[0]], live_enc[0])
+
+    return result[0]
